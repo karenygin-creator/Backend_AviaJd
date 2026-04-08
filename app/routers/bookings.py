0959ac47ec53a963b0e83c2524f6e9d2619d6906
karenygin-creator <karenygin@gmail.com>
2026-04-08 19:37:17 +0300

    bookings+

--- app/routers/bookings.py
+++ app/routers/bookings.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.schemas.booking import BookingCreate
@@ -8,7 +8,19 @@ from database import get_db
 router = APIRouter(prefix="/bookings", tags=["Bookings"])
 @router.post("/")
 async def create_booking(data:BookingCreate,db:AsyncSession=Depends(get_db)):
-    return await BookingService.create_booking(db,data)
+    booking = await BookingService.create_booking(db,data)
+    if not booking:
+        raise HTTPException(status_code=404, detail="Booking not found")
+    return booking
 @router.get("/")
 async def get_booking(db:AsyncSession=Depends(get_db)):
     return await BookingService.get_all(db)
+@router.delete("/{booking_id}")
+async def delete_booking(booking_id:int,db:AsyncSession=Depends(get_db)):
+    booking=await BookingService.delete_booking(db,booking_id)
+    if not booking:
+        raise HTTPException(status_code=404, detail="Booking not found")
+    return{
+        "message": "Booking cancelled",
+        "booking": booking
+    }
--- app/services/booking_service.py
+++ app/services/booking_service.py
@@ -1,14 +1,37 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.models.models import Booking
+from app.models.models import Booking, User, Trip
 class BookingService:
     @staticmethod
     async def create_booking(db:AsyncSession,data):
+        user_result=await db.execute(select(User).where(User.id==data.user_id))
+        user=user_result.scalar_one_or_none()
+        if not user:
+            return None
+        trips_result=await db.execute(select(Trip).where(Trip.id==data.trip_id))
+        trip=trips_result.scalar_one_or_none()
+        if not trip:
+            return None
         booking = Booking(**data.dict())
         db.add(booking)
         await db.commit()
+        await db.refresh(booking)
         return booking
     @staticmethod
     async def get_all(db:AsyncSession):
         bookings = await db.execute(select(Booking))
         return bookings.scalars().all()
+    @staticmethod
+    async def delete_booking(db:AsyncSession,booking_id:int):
+        result = await db.execute(select(Booking).where(Booking.id==booking_id))
+        booking = result.scalar_one_or_none()
+        if not booking:
+            return None
+        booking.status="cancelled"
+        await db.commit()
+        await db.refresh(booking)
+        return booking
+    @staticmethod
+    async def get_user_bookings(db:AsyncSession,user_id:int):
+        result=await db.execute(select(Booking).where(Booking.user_id==user_id))
+        return result.scalars().all()
